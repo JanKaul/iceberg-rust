8cacb9f6a900bcc140c6ab7896f15ff3cb9644ce
Jan Kaul <jankaul@mailbox.org>
2023-10-26 08:03:33 +0200

    remove iceberg-c

--- iceberg-c/src/table/transaction.rs
+++
@@ -1,9 +0,0 @@
-use iceberg_rust::table::transaction::TableTransaction;
-
-use crate::block_on;
-
-/// Commit transaction freeing its memmory
-#[no_mangle]
-pub extern "C" fn table_transaction_commit(transaction: Box<TableTransaction>) {
-    block_on(transaction.commit()).unwrap();
-}
