653d0bcc05ea83f215eed591eb9a8fa09dd48af2
Jan Kaul <jankaul@mailbox.org>
2023-11-10 17:22:20 +0100

    remove file format enum

--- iceberg-rust/src/file_format/mod.rs
+++ iceberg-rust/src/file_format/mod.rs
@@ -2,12 +2,4 @@
  * Helper functions for different file formats.
 */
 
-use ::parquet::format::FileMetaData;
 pub mod parquet;
-
-#[derive(Debug, Clone)]
-/// Metadata for a datafile
-pub enum DatafileMetadata {
-    /// Metadata for a parquet datafile
-    Parquet(FileMetaData),
-}
