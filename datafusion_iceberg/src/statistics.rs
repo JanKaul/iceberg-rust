e42f4a78e7ad2761bb6805bca857c7e073ad7459
Jan Kaul <jankaul@mailbox.org>
2023-11-17 08:24:37 +0100

    use datafiles for datafusion statistics

--- datafusion_iceberg/src/statistics.rs
+++ datafusion_iceberg/src/statistics.rs
@@ -1,7 +1,7 @@
 use std::ops::Deref;
 
 use datafusion::physical_plan::{ColumnStatistics, Statistics};
-use iceberg_rust::catalog::tabular::Tabular;
+use iceberg_rust::{catalog::tabular::Tabular, table::Table};
 
 use crate::error::Error;
 
@@ -10,102 +10,41 @@ use super::table::DataFusionTable;
 impl DataFusionTable {
     pub(crate) async fn statistics(&self) -> Result<Statistics, Error> {
         match self.tabular.read().await.deref() {
-            Tabular::Table(table) => {
-                let schema = self
-                    .snapshot_range
-                    .1
-                    .and_then(|snapshot_id| table.metadata().schema(snapshot_id).ok().cloned())
-                    .unwrap_or_else(|| table.current_schema(None).unwrap().clone());
-                table
-                    .manifests(self.snapshot_range.0, self.snapshot_range.1)
-                    .await?
-                    .iter()
-                    .try_fold(
-                        Statistics {
-                            num_rows: Some(0),
-                            total_byte_size: None,
-                            column_statistics: Some(vec![
-                                ColumnStatistics {
-                                    null_count: None,
-                                    max_value: None,
-                                    min_value: None,
-                                    distinct_count: None
-                                };
-                                schema.fields.fields.len()
-                            ]),
-                            is_exact: true,
-                        },
-                        |acc, x| {
-                            Ok(Statistics {
-                                num_rows: acc.num_rows.zip(x.added_files_count).map(
-                                    |(num_rows, added_files_count)| {
-                                        num_rows + added_files_count as usize
-                                    },
-                                ),
-                                total_byte_size: None,
-                                column_statistics: Some(vec![
-                                    ColumnStatistics {
-                                        null_count: None,
-                                        max_value: None,
-                                        min_value: None,
-                                        distinct_count: None
-                                    };
-                                    schema.fields.fields.len()
-                                ]),
-                                is_exact: true,
-                            })
-                        },
-                    )
-            }
+            Tabular::Table(table) => table_statistics(table, &self.snapshot_range).await,
             Tabular::View(_) => Err(Error::NotSupported("Statistics for views".to_string())),
             Tabular::MaterializedView(mv) => {
                 let table = mv.storage_table(None).await.map_err(Error::from)?;
-                let schema = self
-                    .snapshot_range
-                    .1
-                    .and_then(|snapshot_id| table.metadata().schema(snapshot_id).ok().cloned())
-                    .unwrap_or_else(|| table.current_schema(None).unwrap().clone());
-                table
-                    .manifests(self.snapshot_range.0, self.snapshot_range.1)
-                    .await?
-                    .iter()
-                    .try_fold(
-                        Statistics {
-                            num_rows: Some(0),
-                            total_byte_size: None,
-                            column_statistics: Some(vec![
-                                ColumnStatistics {
-                                    null_count: None,
-                                    max_value: None,
-                                    min_value: None,
-                                    distinct_count: None
-                                };
-                                schema.fields.fields.len()
-                            ]),
-                            is_exact: true,
-                        },
-                        |acc, x| {
-                            Ok(Statistics {
-                                num_rows: acc.num_rows.zip(x.added_files_count).map(
-                                    |(num_rows, added_files_count)| {
-                                        num_rows + added_files_count as usize
-                                    },
-                                ),
-                                total_byte_size: None,
-                                column_statistics: Some(vec![
-                                    ColumnStatistics {
-                                        null_count: None,
-                                        max_value: None,
-                                        min_value: None,
-                                        distinct_count: None
-                                    };
-                                    schema.fields.fields.len()
-                                ]),
-                                is_exact: true,
-                            })
-                        },
-                    )
+                table_statistics(&table, &self.snapshot_range).await
             }
         }
     }
+}
+
+async fn table_statistics(
+    table: &Table,
+    snapshot_range: &(Option<i64>, Option<i64>),
+) -> Result<Statistics, Error> {
+    let schema = snapshot_range
+        .1
+        .and_then(|snapshot_id| table.metadata().schema(snapshot_id).ok().cloned())
+        .unwrap_or_else(|| table.current_schema(None).unwrap().clone());
+    let manifests = table.manifests(snapshot_range.0, snapshot_range.1).await?;
+    let datafiles = table.datafiles(&manifests, None).await?;
+    Ok(datafiles.iter().fold(
+        Statistics {
+            num_rows: Some(0),
+            total_byte_size: None,
+            column_statistics: Some(vec![
+                ColumnStatistics {
+                    null_count: None,
+                    max_value: None,
+                    min_value: None,
+                    distinct_count: None
+                };
+                schema.fields.fields.len()
+            ]),
+            is_exact: true,
+        },
+        |acc, _| acc,
+    ))
 }
