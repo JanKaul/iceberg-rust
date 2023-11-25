3aeca1def0898d3cc52713eeb4a0937b297fc813
Jan Kaul <jankaul@mailbox.org>
2023-11-25 10:31:00 +0100

    duplicate sql catalog

--- iceberg-catalog-sql/src/lib.rs
+++ iceberg-catalog-sql/src/lib.rs
@@ -260,6 +260,13 @@ impl Catalog for SqlCatalog {
 }
 
 #[cfg(test)]
+impl SqlCatalog {
+    pub fn duplicate(&self, name: &str) -> Self {
+        Self { name: name.to_owned(), connection: self.connection.clone(), object_store: self.object_store.clone() }
+}
+}
+
+#[cfg(test)]
 pub mod tests {
     use iceberg_rust::{spec::{
         schema::Schema,
