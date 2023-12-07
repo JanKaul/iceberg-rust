489fae1a9b98a5a04af238ab8d8b5f361e72e28a
Jan Kaul <jankaul@mailbox.org>
2023-12-07 11:43:16 +0100

    add objectstore builder

--- iceberg-rust/src/catalog/bucket.rs
+++ iceberg-rust/src/catalog/bucket.rs
@@ -2,6 +2,13 @@
 Defining the [Bucket] struct for specifying buckets for the ObjectStore.
 */
 
+use std::sync::Arc;
+
+use object_store::{
+    aws::AmazonS3Builder, gcp::GoogleCloudStorageBuilder, local::LocalFileSystem, memory::InMemory,
+    ObjectStore,
+};
+
 use crate::error::Error;
 
 /// Type for buckets for different cloud providers
@@ -30,5 +37,43 @@ pub fn parse_bucket(path: &str) -> Result<Bucket, Error> {
             .ok_or(Error::NotFound(format!("Table"), format!("location")))
     } else {
         Ok(Bucket::Local)
+    }
+}
+
+/// A wrapper for ObjectStore builders that can be used as a template to generate an ObjectStore given a particular bucket.
+#[derive(Debug)]
+pub enum ObjectStoreBuilder {
+    /// AWS s3 builder
+    S3(AmazonS3Builder),
+    /// Google Cloud Storage builder
+    GCS(GoogleCloudStorageBuilder),
+    /// Filesystem builder
+    Filesystem(Arc<LocalFileSystem>),
+    /// In memory builder
+    Memory(Arc<InMemory>),
+}
+
+impl ObjectStoreBuilder {
+    /// Create objectstore from template
+    pub fn build(&self, bucket: Bucket) -> Result<Arc<dyn ObjectStore>, Error> {
+        match (bucket, self) {
+            (Bucket::S3(bucket), Self::S3(builder)) => Ok::<_, Error>(Arc::new(
+                builder
+                    .clone()
+                    .with_bucket_name(bucket)
+                    .build()
+                    .map_err(Error::from)?,
+            )),
+            (Bucket::GCS(bucket), Self::GCS(builder)) => Ok::<_, Error>(Arc::new(
+                builder
+                    .clone()
+                    .with_bucket_name(bucket)
+                    .build()
+                    .map_err(Error::from)?,
+            )),
+            (Bucket::Local, Self::Filesystem(object_store)) => Ok(object_store.clone()),
+            (Bucket::Local, Self::Memory(object_store)) => Ok(object_store.clone()),
+            _ => Err(Error::NotSupported("Object store protocol".to_owned())),
+        }
     }
 }
