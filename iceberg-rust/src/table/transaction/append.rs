037b9824682e54ae61fa705116decaa001d51b17
Jan Kaul <jankaul@mailbox.org>
2024-10-25 08:38:03 +0200

    fix public function

--- iceberg-rust/src/table/transaction/append.rs
+++ iceberg-rust/src/table/transaction/append.rs
@@ -10,7 +10,7 @@ use crate::{
 
 /// Split sets of datafiles depending on their partition_values
 #[allow(clippy::type_complexity)]
-pub(crate) fn split_datafiles_once(
+pub fn split_datafiles_once(
     files: impl Iterator<Item = Result<ManifestEntry, Error>>,
     rect: Rectangle,
     names: &[&str],
