8f80dd1f163e8f203b2ff00c5f595c58eb5a08e3
Jan Kaul <jankaul@mailbox.org>
2025-01-09 15:19:24 +0100

    update refresh_materialized_view

--- datafusion_iceberg/src/materialized_view/delta_queries/delta_node.rs
+++ datafusion_iceberg/src/materialized_view/delta_queries/delta_node.rs
@@ -56,10 +56,71 @@ impl UserDefinedLogicalNodeCore for PosDeltaNode {
 }
 
 impl PosDeltaNode {
-    pub fn new(plan: LogicalPlan) -> Self {
+    pub fn new(plan: Arc<LogicalPlan>) -> Self {
+        Self { input: plan }
+    }
+
+    pub(crate) fn into_logical_plan(self) -> LogicalPlan {
+        LogicalPlan::Extension(Extension {
+            node: Arc::new(self),
+        })
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, PartialOrd)]
+pub struct NegDeltaNode {
+    pub input: Arc<LogicalPlan>,
+}
+
+impl Debug for NegDeltaNode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.fmt_for_explain(f)
+    }
+}
+
+impl UserDefinedLogicalNodeCore for NegDeltaNode {
+    fn name(&self) -> &str {
+        "NegDelta"
+    }
+
+    fn inputs(&self) -> Vec<&LogicalPlan> {
+        vec![&self.input]
+    }
+
+    fn schema(&self) -> &DFSchemaRef {
+        self.input.schema()
+    }
+
+    fn expressions(&self) -> Vec<Expr> {
+        vec![]
+    }
+
+    fn fmt_for_explain(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "NegDelta")
+    }
+
+    fn from_template(&self, exprs: &[Expr], inputs: &[LogicalPlan]) -> Self {
+        assert_eq!(inputs.len(), 1, "input size inconsistent");
+        assert_eq!(exprs.len(), 0, "expression size inconsistent");
         Self {
-            input: Arc::new(plan),
+            input: Arc::new(inputs[0].clone()),
         }
+    }
+
+    fn with_exprs_and_inputs(
+        &self,
+        _exprs: Vec<Expr>,
+        inputs: Vec<LogicalPlan>,
+    ) -> datafusion::error::Result<Self> {
+        Ok(Self {
+            input: Arc::new(inputs[0].clone()),
+        })
+    }
+}
+
+impl NegDeltaNode {
+    pub fn new(plan: Arc<LogicalPlan>) -> Self {
+        Self { input: plan }
     }
 
     pub(crate) fn into_logical_plan(self) -> LogicalPlan {
