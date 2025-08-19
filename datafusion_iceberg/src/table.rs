a0e8bde5d59c0c2adf913c532907bc3c9ff49300
JanKaul <jankaul@mailbox.org>
2025-08-19 13:59:20 +0200

    fix compression

--- datafusion_iceberg/src/table.rs
+++ datafusion_iceberg/src/table.rs
@@ -1117,7 +1117,7 @@ pub async fn write_parquet_with_sink(
     };
 
     let mut parquet_options = TableParquetOptions::default();
-    parquet_options.set("datafusion.execution.parquet.compression", "zstd(1)")?;
+    parquet_options.set("compression", "zstd(1)")?;
 
     let sink = ParquetSink::new(config, parquet_options);
 
