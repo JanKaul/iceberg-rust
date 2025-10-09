86b4b6e368e73118d796185e6554d769614411e8
JanKaul <jankaul@mailbox.org>
2025-10-09 09:49:01 +0200

    add add and remove logic

--- datafusion_iceberg/src/optimizer/query_graph.rs
+++ datafusion_iceberg/src/optimizer/query_graph.rs
@@ -1,34 +1,57 @@
-use slotmap::{new_key_type, SlotMap};
+use std::sync::Arc;
+
+use datafusion_expr::{Join, LogicalPlan};
+use slotmap::{basic::Iter, new_key_type, SlotMap};
 
 new_key_type! {
     pub struct NodeId;
 }
 
-struct Node<T> {
+pub(crate) struct Node<T> {
     data: T,
     connections: Vec<EdgeId>,
 }
+
+impl<N> Node<N> {
+    pub(crate) fn connections(&self) -> &[EdgeId] {
+        &self.connections
+    }
+
+    pub(crate) fn neighbours<E>(&self, query_graph: &QueryGraph<N, E>) -> Vec<NodeId> {
+        todo!()
+    }
+}
+
+pub(crate) type QueryNode = Node<Arc<LogicalPlan>>;
 
 new_key_type! {
     pub struct EdgeId;
 }
 
-struct Edge<T> {
+pub(crate) struct Edge<T> {
     nodes: [NodeId; 2],
     data: T,
 }
 
-struct QueryGraph<N, E> {
+pub(crate) type QueryEdge = Edge<Join>;
+
+pub(crate) struct QueryGraph<N, E> {
     nodes: SlotMap<NodeId, Node<N>>,
     edges: SlotMap<EdgeId, Edge<E>>,
 }
 
 impl<N, E> QueryGraph<N, E> {
-    fn add_node(&mut self, data: N) -> NodeId {
-        self.nodes.insert(Node {
-            data,
-            connections: Vec::new(),
-        })
+    pub(crate) fn add_node(&mut self, other: NodeId, node_data: N, edge_data: E) -> Option<NodeId> {
+        if self.nodes.contains_key(other) {
+            let new_id = self.nodes.insert(Node {
+                data: node_data,
+                connections: Vec::new(),
+            });
+            self.add_edge(new_id, other, edge_data);
+            Some(new_id)
+        } else {
+            None
+        }
     }
 
     fn add_edge(&mut self, from: NodeId, to: NodeId, data: E) -> Option<EdgeId> {
@@ -47,5 +70,52 @@ impl<N, E> QueryGraph<N, E> {
         } else {
             None
         }
+    }
+
+    pub(crate) fn remove_node(&mut self, node_id: NodeId) -> Option<N> {
+        if let Some(node) = self.nodes.remove(node_id) {
+            // Remove all edges connected to this node
+            for edge_id in &node.connections {
+                if let Some(edge) = self.edges.remove(*edge_id) {
+                    // Remove the edge from the other node's connections
+                    for other_node_id in edge.nodes {
+                        if other_node_id != node_id {
+                            if let Some(other_node) = self.nodes.get_mut(other_node_id) {
+                                other_node.connections.retain(|id| id != edge_id);
+                            }
+                        }
+                    }
+                }
+            }
+            Some(node.data)
+        } else {
+            None
+        }
+    }
+
+    fn remove_edge(&mut self, edge_id: EdgeId) -> Option<E> {
+        if let Some(edge) = self.edges.remove(edge_id) {
+            // Remove the edge from both nodes' connections
+            for node_id in edge.nodes {
+                if let Some(node) = self.nodes.get_mut(node_id) {
+                    node.connections.retain(|id| *id != edge_id);
+                }
+            }
+            Some(edge.data)
+        } else {
+            None
+        }
+    }
+
+    fn nodes(&self) -> Iter<'_, NodeId, Node<N>> {
+        self.nodes.iter()
+    }
+
+    fn get_node(&self, key: NodeId) -> Option<&Node<N>> {
+        self.nodes.get(key)
+    }
+
+    fn get_edge(&self, key: EdgeId) -> Option<&Edge<E>> {
+        self.edges.get(key)
     }
 }
