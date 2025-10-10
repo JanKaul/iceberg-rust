7950a42110e48a355a30658928b86fb21790413c
JanKaul <jankaul@mailbox.org>
2025-10-10 14:29:48 +0200

    normalize

--- datafusion_iceberg/src/optimizer/precedence_graph.rs
+++ datafusion_iceberg/src/optimizer/precedence_graph.rs
@@ -10,9 +10,9 @@ use crate::{
     },
 };
 
-struct PrecedenceNode<'graph> {
+struct PrecedenceTreeNode<'graph> {
     query_nodes: Vec<NodeId>,
-    children: Vec<PrecedenceNode<'graph>>,
+    children: Vec<PrecedenceTreeNode<'graph>>,
     // T in Ibaraki,Kameda
     cardinality: usize,
     // C in Ibaraki,Kameda
@@ -20,14 +20,14 @@ struct PrecedenceNode<'graph> {
     query_graph: &'graph QueryGraph,
 }
 
-impl<'graph> PrecedenceNode<'graph> {
+impl<'graph> PrecedenceTreeNode<'graph> {
     pub(crate) fn from_query_graph(
         graph: &'graph QueryGraph,
         root_id: NodeId,
     ) -> Result<Self, Error> {
         let mut remaining: HashSet<NodeId> = graph.nodes().map(|(x, _)| x).collect();
         remaining.remove(&root_id);
-        PrecedenceNode::from_query_node(root_id, 1.0, graph, &mut remaining)
+        PrecedenceTreeNode::from_query_node(root_id, 1.0, graph, &mut remaining)
     }
 
     fn from_query_node(
@@ -40,8 +40,8 @@ impl<'graph> PrecedenceNode<'graph> {
             .get_node(node_id)
             .ok_or(IcebergError::NotFound("Root node".to_owned()))?;
         let input_cardinality = Self::cardinality(&node.data).unwrap_or(1);
-        let connections = node.connections();
-        let children = connections
+        let children = node
+            .connections()
             .iter()
             .filter_map(|edge| {
                 if let Some(edge) = query_graph.get_edge(*edge) {
@@ -52,7 +52,7 @@ impl<'graph> PrecedenceNode<'graph> {
                     {
                         remaining.remove(&other);
                         let selectivity = Self::selectivity(&edge.data);
-                        Some(PrecedenceNode::from_query_node(
+                        Some(PrecedenceTreeNode::from_query_node(
                             other,
                             selectivity,
                             query_graph,
@@ -66,7 +66,7 @@ impl<'graph> PrecedenceNode<'graph> {
                 }
             })
             .collect::<Result<Vec<_>, Error>>()?;
-        Ok(PrecedenceNode {
+        Ok(PrecedenceTreeNode {
             query_nodes: vec![node_id],
             children,
             cardinality: (selectivity * input_cardinality as f64) as usize,
@@ -78,6 +78,51 @@ impl<'graph> PrecedenceNode<'graph> {
     fn rank(&self) -> f64 {
         (self.cardinality - 1) as f64 / self.cost
     }
+
+    fn normalize(&mut self) {
+        match self.children.len() {
+            0 => (),
+            1 => {
+                if self.children[0].rank() < self.rank() {
+                    // Create normalized sequence with child node
+                    let mut child = self.children.pop().unwrap();
+                    self.cost += self.cardinality as f64 * child.cost;
+                    self.cardinality *= child.cardinality;
+                    self.query_nodes.append(&mut child.query_nodes);
+                    self.children = child.children;
+                    self.normalize();
+                } else {
+                    self.children[0].normalize();
+                }
+            }
+            _ => {
+                for child in &mut self.children {
+                    // Normalize child trees into chains
+                    child.normalize();
+                }
+                let mut children = std::mem::take(&mut self.children).into_iter();
+                // Merge child chains into single chain
+                let left = children.next().unwrap();
+                let child = children.fold(left, Self::merge);
+
+                self.children = vec![child]
+            }
+        }
+    }
+
+    fn merge(self, other: PrecedenceTreeNode<'graph>) -> Self {
+        let (mut first, second) = if self.rank() < other.rank() {
+            (self, other)
+        } else {
+            (other, self)
+        };
+        if first.children.is_empty() {
+            first.children = vec![second];
+        } else {
+            first.children = vec![first.children.pop().unwrap().merge(second)];
+        }
+        first
+    }
 }
 
-impl<'graph> CostEstimator for PrecedenceNode<'graph> {}
+impl<'graph> CostEstimator for PrecedenceTreeNode<'graph> {}
