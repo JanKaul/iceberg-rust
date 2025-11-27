1bf3c222c4dbd7c9736cac30261d34e73cc3e7a5
Luís Lizardo <luis.lizardo@enterprisedb.com>
2025-11-27 16:02:55 +0100

    fix: update Azure bucket URL format to use abfss scheme

--- iceberg-rust/src/object_store/mod.rs
+++ iceberg-rust/src/object_store/mod.rs
@@ -42,7 +42,7 @@ impl Display for Bucket<'_> {
             Bucket::S3(s) => write!(f, "s3://{s}"),
             Bucket::GCS(s) => write!(f, "gs://{s}"),
             Bucket::Azure { account, container } => {
-                write!(f, "abfs://{container}@{account}.dfs.core.windows.net/")
+                write!(f, "abfss://{container}@{account}.dfs.core.windows.net/")
             }
             Bucket::Local => write!(f, ""),
         }
