069bda868356febe3a58daeb2867a0d5158731a4
osipovartem <artemosipov.dev@gmail.com>
2025-12-22 12:48:51 +0300

    Set corrent existing rows count

--- iceberg-rust/src/table/manifest.rs
+++ iceberg-rust/src/table/manifest.rs
@@ -381,10 +381,14 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
         manifest.sequence_number = table_metadata.last_sequence_number + 1;
 
         manifest.existing_files_count = Some(
-            manifest.existing_files_count.unwrap_or(0) + manifest.added_files_count.unwrap_or(0),
+            manifest.existing_files_count.unwrap_or(0) + manifest.added_files_count.unwrap_or(0)
+        );
+        manifest.existing_rows_count = Some(
+            manifest.existing_rows_count.unwrap_or(0) + manifest.added_rows_count.unwrap_or(0)
         );
 
         manifest.added_files_count = None;
+        manifest.added_rows_count = None;
 
         Ok(ManifestWriter {
             manifest,
@@ -440,6 +444,8 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
 
         let mut writer = AvroWriter::new(schema, Vec::new());
         let mut filtered_stats = FilteredManifestStats::default();
+        let mut existing_files  = 0i32;
+        let mut existing_rows = 0i64;
 
         writer.add_user_metadata(
             "format-version".to_string(),
@@ -504,6 +510,8 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
                 if entry.snapshot_id().is_none() {
                     *entry.snapshot_id_mut() = Some(manifest.added_snapshot_id);
                 }
+                existing_files += 1;
+                existing_rows += entry.data_file().record_count();
                 Some(to_value(entry).unwrap())
             } else {
                 if *entry.data_file().content() == Content::Data {
@@ -517,11 +525,11 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
 
         manifest.sequence_number = table_metadata.last_sequence_number + 1;
 
-        manifest.existing_files_count = Some(
-            manifest.existing_files_count.unwrap_or(0) + manifest.added_files_count.unwrap_or(0),
-        );
+        manifest.existing_files_count = Some(existing_files);
+        manifest.existing_rows_count = Some(existing_rows);
 
         manifest.added_files_count = None;
+        manifest.added_rows_count = None;
 
         Ok((
             ManifestWriter {
