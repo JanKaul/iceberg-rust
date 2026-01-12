2781bf0fd67db59cb2041d0d4e6a54a4ab90a7df
Jan Kaul <jankaul@mailbox.org>
2026-01-12 11:45:07 +0100

    fix manifest list unwrap

--- iceberg-rust/src/table/manifest.rs
+++ iceberg-rust/src/table/manifest.rs
@@ -368,10 +368,9 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
                 .map_err(|err| apache_avro::Error::DeserializeValue(err.to_string()))
                 .ok()?;
 
-            if *entry.status() == Status::Deleted {
-                return None;
+            if *entry.status() != Status::Deleted {
+                *entry.status_mut() = Status::Existing;
             }
-            *entry.status_mut() = Status::Existing;
             if entry.sequence_number().is_none() {
                 *entry.sequence_number_mut() = Some(manifest.sequence_number);
             }
@@ -507,10 +506,6 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
                 .unwrap();
 
             // Skip deleted data files
-            if *entry.status() == Status::Deleted {
-                return None;
-            }
-
             if entry.sequence_number().is_none() {
                 *entry.sequence_number_mut() = Some(manifest.sequence_number);
             }
@@ -528,7 +523,10 @@ impl<'schema, 'metadata> ManifestWriter<'schema, 'metadata> {
                 filtered_stats.filtered_entries.push(entry);
                 None
             } else {
-                *entry.status_mut() = Status::Existing;
+                if *entry.status() != Status::Deleted {
+                    *entry.status_mut() = Status::Existing;
+                }
+
                 Some(to_value(entry).unwrap())
             }
         }))?;
